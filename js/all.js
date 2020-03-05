40da320c34cc3203f02026e49f48ef3d677c9370
Wen <yuwen7005@gmail.com>
2020-03-06 04:17:19 +0800

    update navbar

--- js/all.js
+++ js/all.js
@@ -1,19 +1,66 @@
 $(document).ready(function () {
-    
+    //form 驗證
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function validata(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }else {
+                        if (event.target.id === 'nextPageCheckout2'){
+                            event.target.action = '../pages/checkout-2.html';
+                        } else if (event.target.id === 'nextPageCheckout3'){
+                            event.target.action = '../pages/checkout-3.html';
+                        } else if (event.target.id === 'EInvoice'){
+                            event.target.action = '../pages/checkout-success.html';
+                        } else if (event.target.id === 'PInvoice'){
+                            event.target.action = '../pages/checkout-success.html';
+                        };
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
     //Top button 移到最上方
     $('.top').click(function(e){
         e.preventDefault();
         $('html, body').animate({scrollTop:0}, 1000);
     });
+    
+    var lastScrollTop = 110;
     $(window).scroll(function () {
         var windowH = $(window).height();
         var scrollPos = $(window).scrollTop();
+        
+        // Collapse Navbar
+        var navbarCollapse = function(){
+            if (  scrollPos > lastScrollTop){
+                $('#mainNav').addClass('sticky-min');
+            } else{
+                $('#mainNav').removeClass('sticky-min');
+            };
+        };
+        navbarCollapse();
+
+
         //Top button
-        if ( scrollPos > 0 ){
-            $('.top').fadeIn(1000);
-        } else {
-            $('.top').fadeOut(2000);
-        }
+        var topBtn = function(){
+            if ( scrollPos > 0 ){
+                $('.top').fadeIn(1000);
+            } else {
+                $('.top').fadeOut(2000);
+            };
+        };
+        topBtn();
+
+
         // index.html section 顯示
         $('.animatedUp').each(function(){
             var thisPos = $(this).offset().top;
@@ -64,32 +111,4 @@ $(document).ready(function () {
             $('#PInvoice').show();
         }
     });
-    //form 驗證
-    (function () {
-        'use strict';
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function validata(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }else {
-                        if (event.target.id === 'nextPageCheckout2'){
-                            event.target.action = '../pages/checkout-2.html';
-                        } else if (event.target.id === 'nextPageCheckout3'){
-                            event.target.action = '../pages/checkout-3.html';
-                        } else if (event.target.id === 'EInvoice'){
-                            event.target.action = '../pages/checkout-success.html';
-                        } else if (event.target.id === 'PInvoice'){
-                            event.target.action = '../pages/checkout-success.html';
-                        };
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
 });
